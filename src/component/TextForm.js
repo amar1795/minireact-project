0f8d588c65d03392918c2b1039e7c097bf055bde
Amar <jeetamar496@gmail.com>
2023-07-01 00:54:02 +0530

    dark and light mode function added

--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -1,6 +1,40 @@
 import React ,{useState} from 'react'
 
+
 export default function TextForm(props) {
+    const [BtnText, setBtnText] = useState("fa-solid fa-moon fa-2xl");
+
+    const [MyStyle, setMyStyle] = useState({
+        color:"black",
+        backgroundColor:"white"
+    });
+
+    const toggleClick=()=>{
+        if (MyStyle.color==="black") {
+
+            setMyStyle({
+                color:"white",
+            backgroundColor:"black"
+
+            })
+
+            setBtnText("fa-regular fa-moon fa-2xl")
+            
+        }
+        else
+        {
+            setMyStyle({
+                color:"black",
+            backgroundColor:"white"
+
+            })
+
+            setBtnText("fa-solid fa-moon fa-2xl")
+            
+        }
+
+    }
+
 
     const handleUpClick =()=>
     {
@@ -22,18 +56,29 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
+    const handleClearText=()=>{
+        setText("");
+    }
+
     const [text, setText] = useState("Enter the text here");
-    //use state is a Hook which changes the state of "text"
+    //use state is a Hook which changes the state of ""
     return (
     
-        <div> <div>
+        <div> 
+        <div>
         <div className="mb-3">
 
         <h1>{props.heading}</h1>
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={MyStyle} id="myBox" rows="8"></textarea>
         </div>
         <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to uppercase</button>
         <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className='btn btn-primary mx-2' onClick={handleClearText}>Clear Text</button>
+        
+        <i className={BtnText} onClick={toggleClick} style={{cursor:"pointer"}} ></i>
+        
+            
+        
         </div>
 
         <div className="mb-3">
